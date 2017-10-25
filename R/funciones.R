bc35c4c27b0c7b43f4e1eb82ef57da5542c81e1d
Gerardo Luis Alcalá González <galcala@alumni.afi.es>
2017-10-25 19:28:37 +0200

    nuevo paquete

---
+++ R/funciones.R
@@ -0,0 +1,14 @@
+#' @export
+duplica <- function(x){
+  salida = x*2
+  return(salida)
+}
+
+
+divide <- function(uno,dos){
+  return(uno / dos)
+}
+
+suma <- function(uno,dos){
+  return(uno + dos)
+}
